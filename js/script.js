cd9618bed1b2966bb8f9388490f1f0c3ef7077b9
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-06-13 14:56:27 +0300

    header

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let header = $('.header'),
+    scrollPrev = 0;
+
+$(window).scroll(function() {
+    let scrolled = $(window).scrollTop();
+
+    if ( scrolled > 100 && scrolled > scrollPrev ) {
+        header.addClass('out');
+    } else {
+        header.removeClass('out');
+    }
+    scrollPrev = scrolled;
+});
+
+
+
+$('input[name="phone"]').mask("+375(99)999-99-99");
+
+let baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+let newUrl = baseUrl + '?utm_source=yandex&utm_medium=cpc&utm_campaign=%7Bcampaign_name_lat%7D&utm_content=%7Bad_id%7D&utm_term=%7Bkeyword%7D';
+history.pushState(null, null, newUrl);
+
+let utms_names = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'];
+
+utms_names.forEach(name => {
+    let utm_inputs = document.querySelectorAll(`.${name}`);
+    utm_inputs.forEach(input => {
+        input.value = new URL(window.location.href).searchParams.get(`${name}`);
+    });
+});
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu(){
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overlow', 'hidden');
+        } else {
+            $('body').css('overlow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
